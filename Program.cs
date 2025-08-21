13fd8007740715a22914588b71ceda2b4d29df5d
btitzer5 <158374295+btitzer5@users.noreply.github.com>
2025-08-21 12:18:16 -0500

    Added Docker and configured

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Make Kestrel listen on the PORT provided by Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
